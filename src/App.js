728a714cb5b5ba747ed6c2e9c472e7124debc729
Trent Yates <tmgyates94@gmail.com>
2020-11-30 16:39:20 -0500

    added movie posters, ratings, and a background

--- src/App.js
+++ src/App.js
@@ -22,28 +22,39 @@ class App extends React.Component {
       .then(response =>
       {
         // then put just the movies (not everything) into the state
+        console.log(response.data.results)
         this.setState({movies: response.data.results});
       });
 
   }
+  handleClick = () => {
+    //send us to the overview
 
+  }
   render() {
     // let movies = [];
     // for(let i = 0; i < this.state.movies.length; i++)
     // {
     //   movies.push(<h2>{this.state.movies[i].title}</h2>)
     // }
-
+      const imagePath = "https://image.tmdb.org/t/p/w500";
     return (
-      <div className="App">
-        <h1>Check out these movies!!!!!!</h1>
-        {this.state.movies.map(movie =>
-          {
-            return <h2>{movie.title} ({movie.release_date})</h2>;
-          })}
-      </div>
+      
+        <div className="App">
+          <img className="background" src="https://i.pinimg.com/originals/d4/b3/e3/d4b3e3e114a251ed9d7d53e652210e25.jpg"/>
+          <div className="Block">
+          <h1>Check out these movies!!!!!!</h1>
+          {this.state.movies.map(movie =>
+            {
+            
+              return <h2 onClick=""><img className="Image" src={imagePath + movie.poster_path}/><br></br>{movie.title} ({movie.vote_average + "/10"})</h2>;
+            
+            })}
+            </div>
+        </div>
+      
     );
-  }
+  } 
 }
 
 export default App;
